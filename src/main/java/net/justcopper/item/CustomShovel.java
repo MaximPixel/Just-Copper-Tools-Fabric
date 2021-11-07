a0aa63ce5a2c8e86a0fbae97583962bbab3975db
Maxim Cvetkov <maximpixel@yandex.ru>
2021-11-07 22:43:10 +0200

    Removed ChangeGroupSort to avoid crash

--- src/main/java/net/justcopper/item/CustomShovel.java
+++ src/main/java/net/justcopper/item/CustomShovel.java
@@ -1,20 +1,10 @@
 package net.justcopper.item;
 
-import net.minecraft.item.*;
-import net.minecraft.util.collection.DefaultedList;
+import net.minecraft.item.ShovelItem;
+import net.minecraft.item.ToolMaterial;
 
-public class CustomShovel extends ShovelItem implements ChangeGroupSort {
+public class CustomShovel extends ShovelItem {
     public CustomShovel(ToolMaterial toolMaterial, float f, float g, Settings settings) {
         super(toolMaterial, f, g, settings);
-    }
-
-    @Override
-    public void appendStacks(ItemGroup group, DefaultedList<ItemStack> stacks) {
-        appendAndSort(group, stacks);
-    }
-
-    @Override
-    public Item getSortPrevItem() {
-        return Items.NETHERITE_HOE;
     }
 }
